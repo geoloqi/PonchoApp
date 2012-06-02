8f8ac515c345a77b25c54ec7a0db6391732da510
Kyle Drake <kyledrake@gmail.com>
2012-06-02 10:32:46 -0700

    Wire in config

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'bundler/setup'
 Bundler.require
 
+configure do
+  @darksky = Darksky::API.new ENV['DARKSKY_KEY']
+end
+
 get '/' do
+  'This is the Location Barometer service. You should not need to access it directly.'
+end
+
+post '/callback' do
+  
 end
